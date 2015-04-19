7ae0558c097b657059dd6aae741b8611c8124032
austinsamsel <austinsamsel@gmail.com>
2015-04-19 14:36:34 -0400

    combine js into one file

--- source/javascripts/upstate.js
+++ source/javascripts/upstate.js
@@ -1,3 +1,12 @@
+// source/js/all.js
+//= require jquery
+//= require fullpage.js/jquery.fullPage
+//= require waypoints/lib/jquery.waypoints
+//= require typed.js/js/typed
+//= require blazy.min.js
+//= require garlicjs/dist/garlic.min.js
+//= require parsleyjs/dist/parsley
+
     $(document).ready(function() {
 
         var navMenu = function(){
